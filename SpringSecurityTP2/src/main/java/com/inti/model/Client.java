05d02272de0f3b6b70565fbd039eb37d071782d8
theod <theod@DESKTOP-VBIVKTD>
2023-03-17 14:55:47 +0100

    Ajout des liaisons entre les tables

--- SpringSecurityTP2/src/main/java/com/inti/model/Client.java
+++ SpringSecurityTP2/src/main/java/com/inti/model/Client.java
@@ -1,9 +1,16 @@
 package com.inti.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.ToString.Exclude;
 
 @Entity
 @Table
@@ -13,11 +20,17 @@ public class Client {
     public Client() {
     }
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private int id;
     private String nom;
     private String prenom;
     private String adresse;
     private String email;
     private String tel;
+    
+    @OneToMany
+    @Exclude
+    private List<Chambre> listeChambres;
 
 }
