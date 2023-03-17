6eb6a96424e88901b5eca539c9e50ababe43008a
lucieargaud <lucieargaud@gmail.com>
2023-03-17 14:23:17 +0100

    Création projet

---
+++ SpringSecurityTP2/src/main/java/com/inti/SpringSecurityTp2Application.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTp2Application.class, args);
+	}
+
+}
